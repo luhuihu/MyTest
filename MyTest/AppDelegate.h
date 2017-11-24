1828932135171b06eb9e4b67493c47a36ed6ebd8
HuangQiang11 <ys@ys-mac-mini.local>
2017-11-24 15:53:21 +0800

    Initial Commit

---
+++ MyTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyTest
+//
+//  Created by ys on 2017/11/24.
+//  Copyright © 2017年 luhh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyTest
+//
+//  Created by ys on 2017/11/24.
+//  Copyright © 2017年 luhh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
